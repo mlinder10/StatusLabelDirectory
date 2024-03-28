56ded98ee9149524c8e3d5420daee688a83c3c58
Matt Linder <linder2015@outlook.com>
2024-03-27 22:47:01 -0400

    update

--- src/hooks/useMonday.tsx
+++ src/hooks/useMonday.tsx
@@ -16,68 +16,75 @@ export default function useMonday() {
     });
 
     async function fetchData() {
-      const data = await monday.api(`
-      query {
-        boards(ids: [${bid}]) {
-          columns {
-            id
-            type
-            settings_str
+      try {
+        const data = await monday.api(`
+        query {
+          boards(ids: [${bid}]) {
+            columns {
+              id
+              type
+              settings_str
+            }
           }
         }
-      }
-      `);
+        `);
 
-      let mondayLabels = [];
-      const columns = data.data.boards[0].columns;
-      for (const col of columns) {
-        if (col.type !== "status") continue;
-        const settings: ColumnSettings = JSON.parse(col.settings_str);
-        for (const [key, value] of Object.entries(settings.labels)) {
-          mondayLabels.push({
-            bid: bid,
-            cid: col.id,
-            text: value,
-            color: settings.labels_colors[key as any].color,
-          });
+        let mondayLabels = [];
+        const columns = data.data.boards[0]?.columns;
+        for (const col of columns) {
+          if (col.type !== "status") continue;
+          const settings: ColumnSettings = JSON.parse(col.settings_str);
+          for (const [key, value] of Object.entries(settings.labels)) {
+            mondayLabels.push({
+              bid: bid,
+              cid: col.id,
+              text: value,
+              color: settings.labels_colors[key as any].color,
+            });
+          }
         }
-      }
 
-      const rs = await client.execute({
-        sql: "select * from labels where bid = ?",
-        args: [bid],
-      });
+        const rs = await client.execute({
+          sql: "select * from labels where bid = ?",
+          args: [bid],
+        });
 
-      let labels: Label[] = []
-      for (const label of labels) {
-        let found = false
-        for (const row of rs.rows) {
-          if (row.cid === label.cid && (row.color === label.color || row.text === label.text)) {
-            found = true
+        let labels: Label[] = [];
+        for (const label of labels) {
+          let found = false;
+          for (const row of rs.rows) {
+            if (
+              row.cid === label.cid &&
+              (row.color === label.color || row.text === label.text)
+            ) {
+              found = true;
+              labels.push({
+                bid,
+                cid: row.cid,
+                text: row.text as string,
+                color: row.color as string,
+                notes: row.notes as string,
+                link: row.link as string,
+              });
+            }
+          }
+
+          if (!found) {
             labels.push({
               bid,
-              cid: row.cid,
-              text: row.text as string,
-              color: row.color as string,
-              notes: row.notes as string,
-              link: row.link as string,
-            })
+              cid: label.cid,
+              text: label.text,
+              color: label.color,
+              notes: "",
+              link: "",
+            });
           }
         }
 
-        if (!found) {
-          labels.push({
-            bid,
-            cid: label.cid,
-            text: label.text,
-            color: label.color,
-            notes: "",
-            link: "",
-          })
-        }
+        setLabels(labels);
+      } catch (err) {
+        console.error(err);
       }
-
-      setLabels(labels)
     }
 
     fetchData();

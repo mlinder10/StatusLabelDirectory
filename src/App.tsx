34e7ea89fd0e164dbadb97ce4b773b89f5c42158
Matt Linder <linder2015@outlook.com>
2024-03-27 22:41:58 -0400

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
-import useMonday from "./hooks/useMonday"
+import LabelView from "./components/LabelView";
+import useMonday from "./hooks/useMonday";
 
 export default function App() {
-  const { labels } = useMonday()
+  const { labels } = useMonday();
 
-  return <div>Hello World {bid}</div>
+  return (
+    <div style={{ display: "flex", flexDirection: "column" }}>
+      {labels.map((label) => (
+        <LabelView key={label.text} label={label} />
+      ))}
+    </div>
+  );
 }

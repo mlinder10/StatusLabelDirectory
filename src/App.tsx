e5ef0e06595f3026862902cc2a22ce3b9fda2d0b
Matt Linder <linder2015@outlook.com>
2024-04-01 11:45:20 -0400

    update

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,12 @@ import ColumnView from "./components/ColumnView";
 import useMonday from "./hooks/useMonday";
 
 export default function App() {
-  const { theme, columns } = useMonday();
+  const { theme, columns, updateLabel } = useMonday();
 
   return (
     <div className={theme} style={{ display: "flex", flexDirection: "column" }}>
       {columns.map((col) => (
-        <ColumnView key={col.cid} column={col} />
+        <ColumnView key={col.cid} column={col} updateLabel={updateLabel} />
       ))}
     </div>
   );
--- src/components/ColumnView.tsx
+++ src/components/ColumnView.tsx
@@ -1,18 +1,36 @@
 import { VscArrowRight } from "react-icons/vsc";
 import { Column } from "../config/types";
 import styles from "../styles/column.module.css";
+import LabelView from "./LabelView";
+import { useState } from "react";
 
 type ColumnViewProps = {
   column: Column;
+  updateLabel: (cid: string, ind: string, notes: string, link: string) => void;
 };
 
-export default function ColumnView({ column }: ColumnViewProps) {
+export default function ColumnView({ column, updateLabel }: ColumnViewProps) {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className={styles.column}>
-      <button>
-        <VscArrowRight />
-      </button>
-      <p>{column.cid}</p>
+      <div className={styles.upper}>
+        <button onClick={() => setIsOpen(!open)}>
+          <VscArrowRight />
+        </button>
+        <p>{column.cid}</p>
+      </div>
+      {isOpen && <Labels />}
     </div>
   );
+
+  function Labels() {
+    return (
+      <div>
+        {column.labels.map((label) => (
+          <LabelView key={label.ind} label={label} updateLabel={updateLabel} />
+        ))}
+      </div>
+    );
+  }
 }

b2f7dadc97d4b542794ee46e4ca9bb113c6ce7cf
Matt Linder <linder2015@outlook.com>
2024-04-03 13:47:58 -0400

    update

--- src/components/LabelView.tsx
+++ src/components/LabelView.tsx
@@ -1,14 +1,7 @@
-import { useCallback, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Label } from "../config/types";
 import { postLabel } from "../config/helpers";
 
-function debounce(this: any, func: (...args: any[]) => void, delay: number) {
-  let timeoutId: NodeJS.Timeout;
-  return (...args: any[]) => {
-    clearTimeout(timeoutId);
-    timeoutId = setTimeout(() => func.apply(this, args), delay);
-  };
-}
 
 type LabelViewProps = {
   label: Label;
@@ -19,17 +12,9 @@ export default function LabelView({ label, updateLabel }: LabelViewProps) {
   const [notes, setNotes] = useState(label.notes);
   const [link, setLink] = useState(label.link);
 
-  const debouncedSearch = useCallback(
-    debounce((notes, link) => {
-      postLabel(label, notes, link);
-      updateLabel(label.cid, label.ind, notes, link);
-    }, 1000),
-    []
-  );
-  
-  // useEffect(() => {
-  //   debouncedSearch(notes, link);
-  // }, [notes, link]);
+  useEffect(() => {
+    console.log(notes, link)
+  }, [notes, link]);
   
   return (
     <div style={{ display: "grid", gridTemplateColumns: "1fr 1fr 1fr 1fr" }}>

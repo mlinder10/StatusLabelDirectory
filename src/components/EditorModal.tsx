36fe71d6360673cf201c3c366d891efcf07ce002
Matt Linder <linder2015@outlook.com>
2024-04-06 13:01:22 -0400

    update

--- src/components/EditorModal.tsx
+++ src/components/EditorModal.tsx
@@ -1,14 +1,21 @@
 import { useContext } from "react";
 import { NotesContext } from "../contexts/NotesProvider";
+import Editor from "./Editor";
 
 export default function EditorModal() {
-  const { editing } = useContext(NotesContext);
+  const { editing, setEditing, notes } = useContext(NotesContext);
 
   if (!editing) return null;
 
+  function handleClose() {
+    console.log(notes);
+    setEditing(false);
+  }
+
   return (
-    <div>
-      <input type="text" />
+    <div style={{ position: "absolute", inset: 0, backgroundColor: "#000" }}>
+      <button onClick={handleClose}>Close</button>
+      <Editor />
     </div>
-  )
+  );
 }
